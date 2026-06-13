7fa5757e8be93ad6fe2ff1823459969c6dd3af48
marekw96 <marek@wiecek.pro>
2026-06-13 23:18:23 +0200

    firmware: use separate buffor to receive start_host message

--- main/usb_cdc_main.cpp
+++ main/usb_cdc_main.cpp
@@ -259,6 +259,8 @@ void usb_main(void)
         return;
     }
 
+    example_wifi_init();
+
     start_device message = {
         .id = message_id::START_DEVICE,
         .device_name = {'E','S','P','3','2','-','C','3'},
@@ -278,17 +280,19 @@ void usb_main(void)
         ESP_LOGE(TAG, "Failed to get MAC address");
     }
 
-    uint8_t buffer[256];
+    uint8_t buffer[64] = {0};
     std::span<uint8_t> buffer_span(buffer, sizeof(buffer));
     auto left_span = buffer_utils<start_device>::write(buffer_span, message);
     auto msg_size = buffer_span.size() - left_span.size();
 
+    uint8_t r_buffer[256] = {0};
+
     while (1) {
         push_to_uart_with_synchro(buffer, msg_size);
 
-        int bytes_read = usb_serial_jtag_read_bytes(buffer, sizeof(buffer), pdMS_TO_TICKS(1000));
+        int bytes_read = usb_serial_jtag_read_bytes(r_buffer, sizeof(r_buffer), pdMS_TO_TICKS(1000));
         if (bytes_read > 0) {
-            message_id id = static_cast<message_id>(buffer[0]);
+            message_id id = static_cast<message_id>(r_buffer[0]);
             if(id == message_id::START_HOST) {
                 break;
             }
@@ -297,7 +301,6 @@ void usb_main(void)
 
     ESP_LOGI(TAG, "Handshake complete");
 
-    example_wifi_init();
     example_espnow_init();
 
     xTaskCreate(jtag_receive_task, "jtag_receive_task", 2048, NULL, 5, NULL);
