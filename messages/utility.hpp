cfa00c40a700de9dd4bd4183b1463679c6e72f36
marekw96 <marek@wiecek.pro>
2026-06-14 11:15:46 +0200

    utility: Use basic_buffer_utils for builtin types

--- messages/utility.hpp
+++ messages/utility.hpp
@@ -3,6 +3,22 @@
 #include <arpa/inet.h>
 #include <span>
 
+static inline char host_to_network(char value) {
+    return value;
+}
+
+static inline char network_to_host(char value) {
+    return value;
+}
+
+static inline uint8_t host_to_network(uint8_t value) {
+    return value;
+}
+
+static inline uint8_t network_to_host(uint8_t value) {
+    return value;
+}
+
 static inline uint32_t host_to_network(uint32_t value) {
     return htonl(value);
 }
@@ -23,124 +39,78 @@ template<typename T>
 struct buffer_utils {
 };
 
-template<>
-struct buffer_utils<uint8_t> {
-    static inline std::span<const uint8_t> read(std::span<const uint8_t> buffer, uint8_t& value) {
-        assert(buffer.size() >= 1);
-        value = buffer[0];
-        return buffer.subspan(1);
+template <typename T>
+struct basic_buffer_utils {
+    static inline std::span<const uint8_t> read(std::span<const uint8_t> buffer, T& value) {
+        assert(buffer.size() >= sizeof(T));
+
+        for(auto i = 0; i < sizeof(T); ++i) {
+            value <<= 8;
+            value |= buffer[i];
+        }
+
+        value = network_to_host(value);
+
+        return buffer.subspan(sizeof(T));
     }
 
-    static inline std::span<uint8_t> write(std::span<uint8_t> buffer, uint8_t value) {
-        assert(buffer.size() >= 1);
-        buffer[0] = value;
-        return buffer.subspan(1);
+    static inline std::span<uint8_t> write(std::span<uint8_t> buffer, T value) {
+        assert(buffer.size() >= sizeof(T));
+
+        value = host_to_network(value);
+
+        for(auto i = 0; i < sizeof(T); ++i) {
+            buffer[sizeof(T) - 1 - i] = value & 0xFF;
+            value >>= 8;
+        }
+
+        return buffer.subspan(sizeof(T));
+    }
+};
+
+template <typename T, size_t S>
+struct basic_buffer_utils_array {
+    static inline std::span<const uint8_t> read(std::span<const uint8_t> buffer, T(&value)[S]) {
+        assert(buffer.size() >= S * sizeof(T));
+
+        for (size_t i = 0; i < S; ++i) {
+            buffer = buffer_utils<T>::read(buffer, value[i]);
+        }
+
+        return buffer;
+    }
+
+    static inline std::span<uint8_t> write(std::span<uint8_t> buffer, const T(&value)[S]) {
+        assert(buffer.size() >= S * sizeof(T));
+
+        for (size_t i = 0; i < S; ++i) {
+            buffer = buffer_utils<T>::write(buffer, value[i]);
+        }
+
+        return buffer;
     }
 };
 
 template<>
-struct buffer_utils<char> {
-    static inline std::span<const uint8_t> read(std::span<const uint8_t> buffer, char& value) {
-        assert(buffer.size() >= 1);
-        value = buffer[0];
-        return buffer.subspan(1);
-    }
+struct buffer_utils<uint8_t> : basic_buffer_utils<uint8_t> {
+};
 
-    static inline std::span<uint8_t> write(std::span<uint8_t> buffer, char value) {
-        assert(buffer.size() >= 1);
-        buffer[0] = value;
-        return buffer.subspan(1);
-    }
+template<>
+struct buffer_utils<char> : basic_buffer_utils<char> {
 };
 
 template<size_t S>
-struct buffer_utils<char[S]> {
-    static inline std::span<const uint8_t> read(std::span<const uint8_t> buffer, char(&value)[S]) {
-        assert(buffer.size() >= S);
-
-        for (size_t i = 0; i < S; ++i) {
-            value[i] = buffer[i];
-        }
-
-        return buffer.subspan(S);
-    }
-
-    static inline std::span<uint8_t> write(std::span<uint8_t> buffer, const char(&value)[S]) {
-        assert(buffer.size() >= S);
-
-        for (size_t i = 0; i < S; ++i) {
-            buffer[i] = value[i];
-        }
-
-        return buffer.subspan(S);
-    }
+struct buffer_utils<char[S]> : basic_buffer_utils_array<char, S> {
 };
 
 template <size_t S>
-struct buffer_utils<uint8_t[S]> {
-    static inline std::span<const uint8_t> read(std::span<const uint8_t> buffer, uint8_t(&value)[S]) {
-        assert(buffer.size() >= S);
-
-        for (size_t i = 0; i < S; ++i) {
-            value[i] = buffer[i];
-        }
-
-        return buffer.subspan(S);
-    }
-
-    static inline std::span<uint8_t> write(std::span<uint8_t> buffer, const uint8_t(&value)[S]) {
-        assert(buffer.size() >= S);
-
-        for (size_t i = 0; i < S; ++i) {
-            buffer[i] = value[i];
-        }
-
-        return buffer.subspan(S);
-    }
+struct buffer_utils<uint8_t[S]> : basic_buffer_utils_array<uint8_t, S> {
 };
 
 template <>
-struct buffer_utils<uint32_t> {
-    static inline std::span<const uint8_t> read(std::span<const uint8_t> buffer, uint32_t& value) {
-        assert(buffer.size() >= 4);
-
-        value = (buffer[0] << 24) | (buffer[1] << 16) | (buffer[2] << 8) | buffer[3];
-        value = network_to_host(value);
-
-        return buffer.subspan(4);
-    }
-
-    static inline std::span<uint8_t> write(std::span<uint8_t> buffer, uint32_t value) {
-        assert(buffer.size() >= 4);
-
-        uint32_t net_value = host_to_network(value);
-        buffer[0] = (net_value >> 24) & 0xFF;
-        buffer[1] = (net_value >> 16) & 0xFF;
-        buffer[2] = (net_value >> 8) & 0xFF;
-        buffer[3] = net_value & 0xFF;
-
-        return buffer.subspan(4);
-    }
+struct buffer_utils<uint32_t> : basic_buffer_utils<uint32_t> {
 };
 
 template <>
-struct buffer_utils<uint16_t> {
-    static inline std::span<const uint8_t> read(std::span<const uint8_t> buffer, uint16_t& value) {
-        assert(buffer.size() >= 2);
-
-        value = (buffer[0] << 8) | buffer[1];
-        value = network_to_host(value);
-
-        return buffer.subspan(2);
-    }
-
-    static inline std::span<uint8_t> write(std::span<uint8_t> buffer, uint16_t value) {
-        assert(buffer.size() >= 2);
-
-        uint16_t net_value = host_to_network(value);
-        buffer[0] = (net_value >> 8) & 0xFF;
-        buffer[1] = net_value & 0xFF;
-
-        return buffer.subspan(2);
-    }
+struct buffer_utils<uint16_t> : basic_buffer_utils<uint16_t> {
 };
